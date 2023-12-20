d7c38cfb3728002bcc1d7fcd61fd8a82f758a82b
Vladimir <vvsergeev87@gmail.com>
2023-12-21 01:37:18 +0300

    feat: create forms

---
+++ src/main/forms.py
@@ -0,0 +1,43 @@
+'''
+Модуль с описанием форм в виде классов
+'''
+from django.forms import ModelForm, TextInput, Textarea
+from main.models import Translate, Summary
+
+
+class TranslateFrom(ModelForm):
+    '''
+    Класс для генерации формы Translate
+    '''
+    class Meta:
+        model = Translate
+        fields = ['translated_article']
+        widgets = {
+            'article': Textarea(attrs={
+                'placeholder':
+                    'Вставьте сюда статью на английском, '
+                    'которую нужно перевести',
+                'class': 'form-control',
+            })
+        }
+        labels = {
+            'translated_article': 'Перевод Статьи'
+        }
+
+
+class SummaryForm(ModelForm):
+    '''Класс для генерации формы Summary'''
+    class Meta:
+        model = Summary
+        fields = ['summarize_article']
+        widgets = {
+            'summarize_article': Textarea(attrs={
+                'placeholder':
+                    'Вставьте сюда статью, '
+                    'по которой хотите получить краткое содержание',
+                'class': 'form-control',
+            })
+        }
+        labels = {
+            'summarize_article': 'Краткое содержание статьи'
+        }
