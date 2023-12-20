1824f0c13ceb187f747d8f56cd7fedb360a40dd3
Vladimir <vvsergeev87@gmail.com>
2023-12-21 01:32:58 +0300

    feat: create models

--- src/main/models.py
+++ src/main/models.py
@@ -1,3 +1,19 @@
+"""
+Модуль содержит классы моделей реализованных в приложении форм.
+"""
 from django.db import models
-
 # Create your models here.
+
+
+class Translate(models.Model):
+    '''
+    Модель формы Translate
+    '''
+    translated_article = models.TextField()
+
+
+class Summary(models.Model):
+    '''
+    Модель формы Summary
+    '''
+    summarize_article = models.TextField()
