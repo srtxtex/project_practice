e05fc63b644cc21cccc88e38933bd5a0551341e6
VirtusTex <srtxtex@gmail.com>
2023-12-21 05:28:37 +0300

    feat: добавление хостов для развертывания в облаке

--- src/sf_app/settings.py
+++ src/sf_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-asbf!rsommq19nhf(wi_pih1r8d$!^uwg7(umx&o_@u5zq8yc@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.47.179']
 
 
 # Application definition
